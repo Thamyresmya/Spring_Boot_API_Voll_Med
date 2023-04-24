d9d680a6218ebfc49e95694fc4592fb536a322e2
Thamyres <thamyresmia@hotmail.com>
2023-04-24 13:56:13 -0300

    add consulta

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,2 +1,7 @@
-package med.voll.api.domain.consulta;public class ConsultaRepository {
+package med.voll.api.domain.consulta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
 }
--- src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -1,2 +1,12 @@
-package med.voll.api.domain.consulta;public record DadosDetalhamentoConsulta() {
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoConsulta(
+        Long id,
+        Long idMedico,
+        Long idPaciente,
+        LocalDateTime data
+
+) {
 }
